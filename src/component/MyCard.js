06b083b86de6095816dca0758a3cfccb51b15e66
amit om kashyap <amit.ece.21@nitap.ac.in>
2024-08-24 07:37:59 +0530

    final

--- src/component/MyCard.js
+++ src/component/MyCard.js
@@ -14,24 +14,24 @@ export default function MyCard(props) {
     const [size, setSize]=useState("")
     const handleAddToCart = async()=>{
         let food = []
-        for (const item of data) {
-          if (item.id === foodItem._id) {
-            food = item;
-            break;
+        // for (const item of data) {
+        //   if (item.id === foodItem._id) {
+        //     food = item;
+        //     break;
+        //   }
+        // }
+        if (food != []) {
+            if (food.size === size) {
+              await dispatch({ type: "UPDATE", id: foodItem._id, price: finalPrice, qty: qty })
+              return
+            }
+            else if (food.size !== size) {
+              await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size,img: props.ImgSrc })
+              console.log("Size different so simply ADD one more to the list")
+              return
+            }
+            return
           }
-        }
-    //     if (food != []) {
-    //         if (food.size === size) {
-    //           await dispatch({ type: "UPDATE", id: foodItem._id, price: finalPrice, qty: qty })
-    //           return
-    //         }
-    //         else if (food.size !== size) {
-    //           await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size,img: props.ImgSrc })
-    //           console.log("Size different so simply ADD one more to the list")
-    //           return
-    //         }
-    //         return
-    //       }
         await dispatch({type:"ADD",id:props.foodItem._id,name:props.foodItem.name,price:finalPrice,qty:qty,size:size})
        console.log(data)
     }
